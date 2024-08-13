560b80da071e1cae9004b4e55214dd06c71163a6
Nguyễn Tống Minh <86721208+minhngt62@users.noreply.github.com>
2024-08-14 00:54:52 +0700

    Add dataset Office-31

---
+++ optimal_transport/experiments/domain_adaptation/datasets/office.py
@@ -0,0 +1,34 @@
+import os
+import torch
+from PIL import Image
+from torch.utils.data import Dataset
+from typing import Tuple
+
+
+class OfficeDataset(Dataset):
+    def __init__(
+        self, 
+        root_dir: str,
+        metadata_file: str,
+        transform=None
+    ):
+        self.data, self.target = [], []
+        with open(metadata_file, "r") as f:
+            for line in f:
+                path, label = line.strip().split(' ')
+                path = os.path.join(root_dir, path)
+                self.data.append(path)
+                self.target.append(int(label))
+        self.transform = transform
+
+    def __getitem__(self, index: int) -> Tuple[torch.Tensor, int]:
+        path, target = self.data[index], self.target[index]
+        img = Image.open(path)
+
+        if self.transform is not None:
+            img = self.transform(img)
+
+        return img, target
+
+    def __len__(self):
+        return len(self.data)
